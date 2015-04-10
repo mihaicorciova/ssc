cc0b46748251490044cffc0c6294d2294f8a1dc6
Koen Olijve <koen.olijve@asml.com>
2015-04-10 14:52:13 +0200

    more styling

--- src/main/java/com/asml/poc/Dashboard.java
+++ src/main/java/com/asml/poc/Dashboard.java
@@ -1,74 +1,48 @@
-package com.asml.wfa.dashboard.gui;
+package com.asml.poc;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
-import org.apache.commons.lang3.StringUtils;
-import org.jboss.weld.environment.se.StartMain;
-import org.jboss.weld.environment.se.WeldContainer;
 
 /**
  * Dashboard, main entry point
- * 
  */
 public class Dashboard extends Application {
-    private static WeldContainer container;
-    private static String modelDirectory;
-    protected DashboardController controller;
+    private Stage stage;
 
-    public static void main(final String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException,
-            IllegalAccessException {
-        /*
-         * TODO: check performance: the application might be bootstrapped twice, check if this is the case or if the startup process can be improved
-         * by letting jboss start the JFX application, or omitting jboss (KOEO)
-         */
-        container = new StartMain(args).go();
-        modelDirectory = extractModelDirectory(args);
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-
+    public static void main(String[] args) {
         launch(args);
     }
 
-    /**
-     * Start the JavaFX application
-     * 
-     * @param stage
-     *            stage
-     */
-    @Override
-    public void start(final Stage stage) {
-        controller = new DashboardController(stage, container);
+    @Override public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        stage.setTitle("POC of Project overview");
+
+        initRootLayout();
+
+        initCentralContent();
     }
 
-    private static String extractModelDirectory(final String[] args) {
-        if (args == null || args.length == 0) {
-            return null;
-        } else {
-            return args[0];
+    private void initCentralContent() {
+        //TODO load central content here...
+    }
+
+    private void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Dashboard.class.getResource("../../../skeleton.fxml"));
+            BorderPane root = (BorderPane) loader.load();
+
+            DashboardController controller = loader.getController();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    /**
-     * Get the model directory set in the command line arguments
-     * 
-     * @return model dir
-     */
-    public static String getModelDirectory() {
-        if (StringUtils.isEmpty(modelDirectory)) {
-            return "";
-        }
-        return modelDirectory;
-    }
-
-    /**
-     * Get the attached controller
-     * 
-     * @return controller that controls the dashboard
-     */
-    public DashboardController getController() {
-        return controller;
     }
 }
--- src/main/java/com/asml/poc/DashboardController.java
+++ src/main/java/com/asml/poc/DashboardController.java
@@ -1,9 +1,12 @@
 package com.asml.poc;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class DashboardController {
 
+    @FXML private Label nodeLabel;
     private Dashboard dashboard;
 
     public DashboardController() {
@@ -12,7 +15,12 @@ public class DashboardController {
     @FXML
     private void initialize(){
         //called after fxml has been loaded
+        System.out.println("q");
+
     }
 
-
+    @FXML
+    public void handle(Event event) {
+        System.out.println("w");
+    }
 }
