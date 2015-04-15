d9bb9b7ea648999e35d56a7f83e1210efa35378d
Catalin Tudorache <catalin.tudorache@asml.com>
2015-04-15 17:02:36 +0200

    updated project. added login dialog.

--- client/src/main/java/com/asml/lis/client/MainController.java
+++ client/src/main/java/com/asml/lis/client/MainController.java
@@ -1,23 +1,29 @@
 package com.asml.lis.client;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Pair;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.asml.lis.client.ui.common.DialogManager;
 
 public class MainController implements Initializable {
 	private static final Logger log = LoggerFactory
 			.getLogger(MainController.class);
 	private static final String MENU_LAYOUT_FILE = "/fxml/Menu.fxml";
-	private static final String SIDE_MENU_LAYOUT_FILE = "/fxml/SideMenuAlternative.fxml";
+	private static final String SIDE_MENU_LAYOUT_FILE = "/fxml/SideMenuNoImages.fxml";
 	private static final String STATUS_BAR_LAYOUT_FILE = "/fxml/StatusBar.fxml";
 	private static final String SIDE_MENU_CSS_FILE = "/styles/side_menu.css";
 	private static final String STATUS_BAR_CSS_FILE = "/styles/status_bar.css";
@@ -30,10 +36,19 @@ public class MainController implements Initializable {
 	private AnchorPane contentContainer;
 	@FXML
 	private AnchorPane statusBarContainer;
+	@FXML
+	private TreeView<?> sideMenuTree;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		log.info("Initializing main controller");
+		
+		// show login dialog
+		Dialog<Pair<String, String>> loginDialog = DialogManager.createNewLoginDialog();
+		Optional<Pair<String, String>> result = loginDialog.showAndWait();
+		if(result.isPresent()) {
+			log.info("username = " + result.get().getKey() + ", password = " + result.get().getValue());
+		}
 
 		// load components
 		try {
@@ -61,7 +76,7 @@ public class MainController implements Initializable {
 			AnchorPane.setRightAnchor(statusLabel, 10.0);
 			statusBarContainer.getChildren().add(statusLabel);
 			statusBarContainer.getStylesheets().add(STATUS_BAR_CSS_FILE);
-
+			
 		} catch (Exception ex) {
 			log.error("failed to load components", ex);
 		}
