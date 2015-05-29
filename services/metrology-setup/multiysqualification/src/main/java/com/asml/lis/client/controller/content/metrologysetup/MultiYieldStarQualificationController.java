6404573d6e50b7efa811cdb3604120314a30b900
bbenga <bogdan.benga@asml.com>
2015-05-29 10:01:09 +0200

    bbenga - Aded Export to PPT Functionality

--- services/metrology-setup/multiysqualification/src/main/java/com/asml/lis/client/controller/content/metrologysetup/MultiYieldStarQualificationController.java
+++ services/metrology-setup/multiysqualification/src/main/java/com/asml/lis/client/controller/content/metrologysetup/MultiYieldStarQualificationController.java
@@ -1,14 +1,15 @@
 package com.asml.lis.client.controller.content.metrologysetup;
 
-import com.asml.lis.client.service.metrologysetup.impl.MultiYSQualificationServiceImpl;
-import com.asml.lis.client.service.metrologysetup.model.GenericModel;
-import com.asml.lis.client.service.metrologysetup.model.MachineData;
-import com.asml.lis.client.service.metrologysetup.model.PlotData;
-import com.asml.lis.client.service.metrologysetup.model.ProfileData;
-import com.asml.wfa.common.guicomponents.widgets.datasetselector.DatasetSelector;
-import com.asml.wfa.metrotools.tooltotoolmatching.gui.widgets.VectorWaferPlot;
+import static com.asml.lis.client.ui.common.AlertMessage.EXPORTING_FILE_IN_USE;
+import static com.asml.lis.client.ui.common.AlertMessage.NO_MATCHING_FILES;
+import static com.asml.lis.client.ui.common.AlertMessage.PPT_EXPORTED;
+import static com.asml.lis.client.ui.common.AlertMessage.PPT_NOT_EXPORTED;
+
 import java.awt.Dimension;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,10 +17,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
+
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingNode;
@@ -30,20 +29,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.util.Callback;
+import javafx.stage.Window;
+
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+
+import org.apache.poi.ss.formula.atp.DateParser;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.asml.lis.client.service.metrologysetup.impl.MultiYSQualificationServiceImpl;
+import com.asml.lis.client.service.metrologysetup.model.GenericModel;
+import com.asml.lis.client.service.metrologysetup.model.MachineData;
+import com.asml.lis.client.service.metrologysetup.model.PlotData;
+import com.asml.lis.client.service.metrologysetup.model.ProfileData;
+import com.asml.lis.client.service.metrologysetup.util.PptExportTools;
+import com.asml.lis.client.ui.common.Constants;
+import com.asml.lis.client.ui.common.UiCommonTools;
+import com.asml.wfa.common.guicomponents.widgets.datasetselector.DatasetSelector;
+import com.asml.wfa.metrotools.tooltotoolmatching.gui.widgets.VectorWaferPlot;
 
 /**
  * FXML Controller class
@@ -58,6 +73,8 @@ public class MultiYieldStarQualificationController extends Application implement
     @FXML
     private Button selectFilesButton;
     @FXML
+    private Button exportPptButton;
+    @FXML
     private ListView selectedFilesListView;
     @FXML
     private Button matchSelectedButton;
@@ -70,7 +87,7 @@ public class MultiYieldStarQualificationController extends Application implement
     @FXML
     private ChoiceBox matchingTypeMachineChoiceBox;
     @FXML
-    private TableView matchingTableView;
+    private TableView<GenericModel> matchingTableView;
     @FXML
     TableColumn<GenericModel, Object> machineNamesTableColumn;
     @FXML
@@ -148,11 +165,83 @@ public class MultiYieldStarQualificationController extends Application implement
                 new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(final ActionEvent e) {
-
-                        createSwingContent(waferPlotContainer, plotContent);
-                        populateMatchingTable(plotContent);
+                        if (plotContent == null || plotContent.isEmpty()) {
+                            
+                            UiCommonTools.getInstance().showAlertDialog(NO_MATCHING_FILES.getTitle(), NO_MATCHING_FILES.getHeader(),
+                                    NO_MATCHING_FILES.getText(), AlertType.WARNING);
+                        } else {
+                            createSwingContent(waferPlotContainer, plotContent);
+                            exportPptButton.setDisable(false);
+                            populateMatchingTable(plotContent);
+                        }
                     }
                 });
+        
+        exportPptButton.setDisable(true);
+        exportPptButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                if (waferPlotContainer.getContent() == null) {
+                    UiCommonTools.getInstance().showAlertDialog(PPT_NOT_EXPORTED.getTitle(), PPT_NOT_EXPORTED.getHeader(),
+                            PPT_NOT_EXPORTED.getText(), AlertType.WARNING);
+                } else {
+                    try {
+                        File exportPptFile =
+                                UiCommonTools.getInstance().getFileByChooser(((Window) exportPptButton.getContextMenu()), "PPT files (*.ppt)",
+                                        ".ppt");
+                        log.debug("Export content to file: " + exportPptFile.getAbsolutePath());
+                        JPanel panelToExport = (JPanel) waferPlotContainer.getContent().getComponents()[0];
+                        
+                        byte[] picture = PptExportTools.getPictureFromJComponent(panelToExport);
+                        
+                        String templatePath = getClass().getResource(Constants.ASML_PPT_TEMPLATE_PATH).getPath();
+                        
+                        log.debug("Creates a new XMLSlideShow from Template file: " + templatePath);
+                        XMLSlideShow ppt = new XMLSlideShow(new FileInputStream(templatePath));
+                        
+                        PptExportTools.addPictureToNewPptSlide(ppt, picture, 0.5, "Export Wafer Plot");
+                        
+                        String[][] content = getTableContent(matchingTableView);
+                        PptExportTools.exportTableToSlide(ppt, matchingTableView, content, "Exported Table");
+
+                        // remove unused last slide which was used as template to create new slides for each exported component
+                        ppt.removeSlide(ppt.getSlides().length - 1);
+
+                        // creating a file object
+                        FileOutputStream out = new FileOutputStream(exportPptFile);
+
+                        // saving the changes to a file
+                        ppt.write(out);
+                        out.close();
+
+                        UiCommonTools.getInstance().showAlertDialog(PPT_EXPORTED.getTitle(), PPT_EXPORTED.getHeader(), PPT_EXPORTED.getText(),
+                                AlertType.INFORMATION);
+
+                    } catch (FileNotFoundException ex) {
+                        UiCommonTools.getInstance().showAlertDialog(EXPORTING_FILE_IN_USE.getTitle(), EXPORTING_FILE_IN_USE.getHeader(),
+                                ex.getMessage() + "\n" + EXPORTING_FILE_IN_USE.getText(), AlertType.ERROR);
+
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private String[][] getTableContent(TableView<GenericModel> fxTable) {
+        String[][] content = new String[fxTable.getItems().size()][fxTable.getColumns().size()];
+        
+        int rowNo = 0;
+        for (GenericModel genericModel : fxTable.getItems()) {
+            content[rowNo][0] = "" + genericModel.getOne();
+            content[rowNo][1] = "" + genericModel.getTwo();
+            content[rowNo][2] = "" + genericModel.getThree();
+            content[rowNo][3] = "" + genericModel.getFour();
+            content[rowNo][4] = "" + genericModel.getFive();
+            rowNo++;
+        }
+        return content;
     }
 
     private void populateListView(List<File> files) {
