c07a71ed1f3d8399691abebf77617b5d796beac1
mihaicorciova <mihai.corciova1@gmail.com>
2015-06-12 21:19:12 +0200

    refactored sumary

--- client/src/main/java/com/ro/ssc/app/client/controller/content/sumary/SumaryController.java
+++ client/src/main/java/com/ro/ssc/app/client/controller/content/sumary/SumaryController.java
@@ -6,18 +6,16 @@
 package com.ro.ssc.app.client.controller.content.sumary;
 
 import com.ro.ssc.app.client.model.commons.Event;
-import com.ro.ssc.app.client.model.commons.ExcelEnum;
 import com.ro.ssc.app.client.model.commons.GenericModel;
 import com.ro.ssc.app.client.model.commons.User;
+import com.ro.ssc.app.client.service.impl.DataProviderImpl;
 import java.io.File;
-import java.io.FileInputStream;
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -30,17 +28,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.joda.time.DateTime;
+import javax.inject.Inject;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
@@ -60,7 +51,6 @@ public class SumaryController implements Initializable {
     private ListView filesListView;
     @FXML
     private TableView sumaryTableView;
-    private Map<String, User> pair = new HashMap<>();
     @FXML
     private TableColumn<GenericModel, Object> dateTableColumn;
     @FXML
@@ -73,6 +63,9 @@ public class SumaryController implements Initializable {
     private TableColumn<GenericModel, Object> departmentTableColumn;
     @FXML
     private TableColumn<GenericModel, Object> eventTableColumn;
+    @Inject
+    private DataProviderImpl service;
+    
     
     /**
      * Initializes the controller class.
@@ -85,6 +78,8 @@ public class SumaryController implements Initializable {
         log.info("Initializing Sumary controller");
 
         final FileChooser fileChooser = new FileChooser();
+        
+        service=new DataProviderImpl();
 
         selectButton.setOnAction(
                 new EventHandler<ActionEvent>() {
@@ -100,11 +95,11 @@ public class SumaryController implements Initializable {
                         //      
                         for (File file : files) {
 
-                            pair = readExcel(file);
+                           service.importUserData(file);
                         }
 
-                        if (!pair.isEmpty()) {
-                            populateMyTable();
+                        if (!service.getUserData().isEmpty()) {
+                            populateMyTable(service.getUserData());
                             log.debug("not emp");
                         }
                     }
@@ -122,7 +117,7 @@ public class SumaryController implements Initializable {
 
     }
 
-    public void populateMyTable() {
+    public void populateMyTable( Map<String, User> pair ) {
 
         dateTableColumn.setCellValueFactory(new PropertyValueFactory<>("one"));
         hourTableColumn.setCellValueFactory(new PropertyValueFactory<>("two"));
@@ -161,60 +156,4 @@ public class SumaryController implements Initializable {
                 new File(System.getProperty("user.home"))
         );
     }
-
-    public Map<String, User> readExcel(File file) {
-        Map<String, User> result = new HashMap<>();
-        List<Event> events = new ArrayList();
-        try {
-            POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(file));
-            HSSFWorkbook wb = new HSSFWorkbook(fs);
-            HSSFSheet sheet = wb.getSheetAt(0);
-            HSSFRow row;
-            HSSFCell cell;
-
-            int rows; // No of rows
-            rows = sheet.getPhysicalNumberOfRows();
-
-            int cols = 0; // No of columns
-            int tmp = 0;
-
-            // This trick ensures that we get the data properly even if it doesn't start from first few rows
-            for (int i = 0; i < 10 || i < rows; i++) {
-                row = sheet.getRow(i);
-                if (row != null) {
-                    tmp = sheet.getRow(i).getPhysicalNumberOfCells();
-                    if (tmp > cols) {
-                        cols = tmp;
-                    }
-                }
-            }
-
-            DateTimeFormatter dtf =  DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss EEEE");
-            for (int r = 1; r < rows; r++) {
-                row = sheet.getRow(r);
-
-                if (row != null) {
-                    try {
-                        
-                        if (result.containsKey(row.getCell(ExcelEnum.USER_NAME.getAsInteger()).toString())) {
-                            events = result.get(row.getCell(ExcelEnum.USER_NAME.getAsInteger()).toString()).getEvents();
-                            events.add(new Event(DateTime.parse(row.getCell(ExcelEnum.TIMESTAMP.getAsInteger()).toString(), dtf), row.getCell(ExcelEnum.DESCRIPTION.getAsInteger()).toString(), row.getCell(ExcelEnum.ADDRESS.getAsInteger()).toString(), Boolean.valueOf(row.getCell(ExcelEnum.PASSED.getAsInteger()).toString())));
-                            result.get(row.getCell(ExcelEnum.USER_NAME.getAsInteger()).toString()).setEvents(events);
-                            log.debug(ExcelEnum.USER_NAME.getAsInteger().toString());
-                        } else {
-
-                            events.add(new Event(DateTime.parse(row.getCell(ExcelEnum.TIMESTAMP.getAsInteger()).toString(), dtf), row.getCell(ExcelEnum.DESCRIPTION.getAsInteger()).toString(), row.getCell(ExcelEnum.ADDRESS.getAsInteger()).toString(), Boolean.valueOf(row.getCell(ExcelEnum.PASSED.getAsInteger()).toString())));
-                            result.put(row.getCell(ExcelEnum.USER_NAME.getAsInteger()).toString(), new User(row.getCell(ExcelEnum.USER_NAME.getAsInteger()).toString().toLowerCase(), row.getCell(ExcelEnum.USER_ID.getAsInteger()).toString(), row.getCell(ExcelEnum.CARD_NO.getAsInteger()).toString(), row.getCell(ExcelEnum.DEPARTMENT.getAsInteger()).toString(), events));
-                        }
-                    } catch (Exception e) {
-  log.error("Exception" + e.getMessage());
-                    }
-                }
-            }
-        } catch (Exception ioe) {
-            log.error("Exception" + ioe.getMessage());
-        }
-        return result;
-    }
-
 }
